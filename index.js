73e8c2b7f22d120dd2eb3b14849729924f099d71
Steven Tang <TangSteven@users.noreply.github.com>
2018-07-31 12:51:51 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
+// ==UserScript==
+// @name         Enable Hidden Facebook Reactions
+// @namespace    https://github.com/TangSteven/Enable-Facebook-Hidden-Reactions
+// @version      1.0
+// @author       Steven Tang
+// @match        https://www.beta.facebook.com/*
+// @run-at document-idle
+// ==/UserScript==
 const HOOK_MODULES = ['UFICentralUpdates'];
 const UPDATE_FEEDBACK_EVENT = 'update-feedback';
 const DEFAULT_SUPPORTED_REACTIONS_COUNT = 6;
 const HIDDEN_REACTION_TYPE_IDS = [11,12,14,15];
 
 function hookFacebookReactions() {
-  if(window.requireLazy) {
-    window.requireLazy(HOOK_MODULES,
+  if(requireLazy) {
+    requireLazy(HOOK_MODULES,
       (UFICentralUpdates) => {
         UFICentralUpdates.subscribe(UPDATE_FEEDBACK_EVENT, (eventName, feedbackObj) => {
           var supportedReactions = feedbackObj.feedbacktarget.supportedreactions;
           if(supportedReactions.length === DEFAULT_SUPPORTED_REACTIONS_COUNT) {
-            supportedReactions = [...supportedReactions, ...HIDDEN_REACTION_TYPE_IDS]; 
+            supportedReactions = [...supportedReactions, ...HIDDEN_REACTION_TYPE_IDS];
             feedbackObj.feedbacktarget.supportedreactions = supportedReactions;
             UFICentralUpdates.inform(UPDATE_FEEDBACK_EVENT, feedbackObj);
           }
@@ -21,4 +29,5 @@ function hookFacebookReactions() {
     console.error('Failed to inject Facebook Reactions hook.');
   }
 }
+
 hookFacebookReactions();
